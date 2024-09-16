f623d0edebb6de41d01852ef1ad2733989c8bc5c
REFKA FARJALLAH <farjallah.refka@gmail.com>
2024-09-16 16:05:17 +0200

    correction j.s

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,8 +1,28 @@
-const btn = document.querySelector('btn');
-btn.addEventListener('click', () => {
+// const btn = document.querySelector('btn');
+// btn.addEventListener('click', () => {
+//     window.scrollTo({
+//         top: 0,
+//         left: 0,
+//         behavior: "smooth"
+//     })
+// })
+
+const btn = document.querySelector('.btn'); // Sélecteur modifié pour inclure le point (class)
+btn.
+btn
+addEventListener('click', () => {
+
+
     window.scrollTo({
         top: 0,
-        left: 0,
-        behavior: "smooth"
-    })
-})
+
+
+    left: 0,
+
+        behavior,
+
+
+    behavior: "smooth"
+    });
+});
+
